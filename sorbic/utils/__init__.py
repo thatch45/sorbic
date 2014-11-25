6e12753a26e4b2de8da3e2e0ef602a46ffba4f03
Thomas S Hatch <thatch45@gmail.com>
2014-11-24 22:14:54 -0700

    Add utils

--- sorbic/utils/__init__.py
+++ sorbic/utils/__init__.py
@@ -2,3 +2,48 @@
 '''
 Utils
 '''
+
+# Import python libs
+import os
+import time
+import struct
+import hashlib
+import binascii
+import datetime
+
+# create a standard epoch so all platforms will count revs from
+# a standard epoch of jan 1 2014
+STD_EPOCH = time.mktime(datetime.datetime(2014, 1, 1).timetuple())
+
+
+def rand_hex_str(size):
+    '''
+    Return a random string of the passed size using hex encoding
+    '''
+    return binascii.hexlify(os.urandom(size/2))
+
+
+def rand_raw_str(size):
+    '''
+    Return a raw byte string of the given size
+    '''
+    return os.urandom(size)
+
+
+def gen_rev():
+    '''
+    Return a revision based on timestamp
+    '''
+    r_time = time.time() - STD_EPOCH
+    return struct.pack('>Q', r_time * 1000000)
+
+
+def get_hash_data(key_hash):
+    '''
+    Return the key hash function and the hash size
+    '''
+    if hasattr(hashlib, key_hash):
+        func = getattr(hashlib, key_hash)
+        size = len(func('some garbage').hexdigest())
+        return func, size
+    raise ValueError('Hash not available')
