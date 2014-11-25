3cd5e9421a45dafac86bf3234d49c1d967002246
Thomas S Hatch <thatch45@gmail.com>
2014-11-24 21:42:36 -0700

    it is ind nto index

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(name=NAME,
           ],
       packages=[
           'sorbic',
-          'sorbic.index',
+          'sorbic.ind',
           'sorbic.stor',
           'sorbic.utils',
           ]
