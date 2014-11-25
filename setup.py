0f7ce001a06ceef9a1b0ea282fa8f611d4d63e27
Thomas S Hatch <thatch45@gmail.com>
2014-11-24 18:37:00 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# Import python libs
+import os
+import sys
+# Import maras libs
+import sorbic
+
+if 'USE_SETUPTOOLS' in os.environ or 'setuptools' in sys.modules:
+    from setuptools import setup
+else:
+    from distutils.core import setup
+
+NAME = 'sorbic'
+DESC = ('Pure python distributed database (engine first)')
+VERSION = sorbic.version
+
+setup(name=NAME,
+      version=VERSION,
+      description=DESC,
+      author='Thomas S Hatch',
+      author_email='thatch@saltstack.com',
+      url='https://github.com/thatch45/sorbic',
+      classifiers=[
+          'Operating System :: OS Independent',
+          'License :: OSI Approved :: Apache Software License',
+          'Programming Language :: Python',
+          'Programming Language :: Python :: 2.6',
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
+          'Development Status :: 2 - Pre-Alpha',
+          'Topic :: Database',
+          'Topic :: Database :: Database Engines/Servers',
+          'Intended Audience :: Developers',
+          ],
+      packages=[
+          'sorbic',
+          'sorbic.index',
+          'sorbic.stor',
+          'sorbic.utils',
+          ]
+      )
