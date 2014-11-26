590a06d1c7156e7cd1d976ecd5bbc2161c6954b8
Thomas S Hatch <thatch45@gmail.com>
2014-11-25 22:05:31 -0700

    Add test for root dir keys

--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -34,3 +34,18 @@ class TestDB(unittest.TestCase):
         self.assertEqual(
                 table_entry['tfn'],
                 os.path.join(root, 'foo', 'bar', 'sorbic_table_0'))
+
+    def test_top_loc(self):
+        w_dir = tempfile.mkdtemp()
+        root = os.path.join(w_dir, 'db_root')
+        db = sorbic.db.DB(root)
+        data = {1:2}
+        key = 'orange'
+        c_key = db.index.raw_crypt_key(key)
+        db.insert(key, data)
+        pull_data = db.get(key)
+        self.assertEqual(data, pull_data)
+        table_entry = db.index.get_table_entry(key, c_key)
+        self.assertEqual(
+                table_entry['tfn'],
+                os.path.join(root, 'sorbic_table_0'))
