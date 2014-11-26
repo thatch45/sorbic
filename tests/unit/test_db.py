643c2423c7c2b1cb546d929d5d911b5fce249569
Thomas S Hatch <thatch45@gmail.com>
2014-11-25 21:58:26 -0700

    iadd test for higherarchy locations

--- tests/unit/test_db.py
+++ tests/unit/test_db.py
@@ -19,3 +19,18 @@ class TestDB(unittest.TestCase):
         db.insert('foo', data)
         pull_data = db.get('foo')
         self.assertEqual(data, pull_data)
+
+    def test_higherarchy(self):
+        w_dir = tempfile.mkdtemp()
+        root = os.path.join(w_dir, 'db_root')
+        db = sorbic.db.DB(root)
+        data = {1:2}
+        key = 'foo/bar/baz'
+        c_key = db.index.raw_crypt_key(key)
+        db.insert(key, data)
+        pull_data = db.get(key)
+        self.assertEqual(data, pull_data)
+        table_entry = db.index.get_table_entry(key, c_key)
+        self.assertEqual(
+                table_entry['tfn'],
+                os.path.join(root, 'foo', 'bar', 'sorbic_table_0'))
