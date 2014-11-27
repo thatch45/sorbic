d0769c5f6fa16ab65fed891c311fee0c084176dc
Thomas S Hatch <thatch45@gmail.com>
2014-11-26 22:54:40 -0700

    Add tiny table tests
    These tests exposed the problem with the hashtable EOF issue

--- tests/unit/test_scale.py
+++ tests/unit/test_scale.py
@@ -12,7 +12,7 @@ import tempfile
 class TestScale(unittest.TestCase):
     '''
     '''
-    def _many(self, **kwargs):
+    def _many(self, entries=100000, **kwargs):
         '''
         run a scale db execution with the given db kwargs
         '''
@@ -20,10 +20,10 @@ class TestScale(unittest.TestCase):
         root = os.path.join(w_dir, 'db_root')
         db = sorbic.db.DB(root, **kwargs)
         data = {1:1}
-        for num in xrange(100000):
+        for num in xrange(entries):
             key = str(num)
             db.insert(key, data)
-        for num in xrange(100000):
+        for num in xrange(entries):
             key = str(num)
             pull_data = db.get(key)
             self.assertEqual(data, pull_data)
@@ -34,3 +34,13 @@ class TestScale(unittest.TestCase):
 
     def test_many_small_table(self):
         self._many(hash_limit=0xffff)
+
+    def test_many_tiny_table(self):
+        self._many(hash_limit=0xfff)
+
+    def _test_too_many(self):
+        '''
+        This is disabled in normal tests, but it is meant to test the upper
+        limits of key entries scale
+        '''
+        self._many(entries=10000000)
