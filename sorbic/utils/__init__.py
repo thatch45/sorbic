5fbe6e7e20048a8b617f8f90a5b1983e2fdba926
Thomas S Hatch <thatch45@gmail.com>
2014-12-12 21:55:13 -0700

    Add traverse_dict_and_list func to utils

--- sorbic/utils/__init__.py
+++ sorbic/utils/__init__.py
@@ -14,7 +14,7 @@ import datetime
 # create a standard epoch so all platforms will count revs from
 # a standard epoch of jan 1 2014
 STD_EPOCH = time.mktime(datetime.datetime(2014, 1, 1).timetuple()) * 1000000
-
+DEFAULT_TARGET_DELIM = ':'
 
 def rand_hex_str(size):
     '''
@@ -36,3 +36,44 @@ def gen_id():
     '''
     r_time = time.time() * 1000000 - STD_EPOCH
     return struct.pack('>HQ', random.randint(0, 65535), r_time).encode('hex')
+
+
+def traverse_dict_and_list(data, key, default=None, delimiter=DEFAULT_TARGET_DELIM):
+    '''
+    Traverse a dict or list using a colon-delimited (or otherwise delimited,
+    using the 'delimiter' param) target string. The target 'foo:bar:0' will
+    return data['foo']['bar'][0] if this value exists, and will otherwise
+    return the dict in the default argument.
+    Function will automatically determine the target type.
+    The target 'foo:bar:0' will return data['foo']['bar'][0] if data like
+    {'foo':{'bar':['baz']}} , if data like {'foo':{'bar':{'0':'baz'}}}
+    then return data['foo']['bar']['0']
+    '''
+    for each in key.split(delimiter):
+        if isinstance(data, list):
+            try:
+                idx = int(each)
+            except ValueError:
+                embed_match = False
+                # Index was not numeric, lets look at any embedded dicts
+                for embedded in (x for x in data if isinstance(x, dict)):
+                    try:
+                        data = embedded[each]
+                        embed_match = True
+                        break
+                    except KeyError:
+                        pass
+                if not embed_match:
+                    # No embedded dicts matched, return the default
+                    return default
+            else:
+                try:
+                    data = data[idx]
+                except IndexError:
+                    return default
+        else:
+            try:
+                data = data[each]
+            except (KeyError, TypeError):
+                return default
+    return data
