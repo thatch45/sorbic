8c70e27a6b9a850bb98f5d7b5b18b3bf35776d22
Thomas S Hatch <thatch45@gmail.com>
2014-12-30 22:55:05 -0700

    Add compress test for key loss

--- tests/unit/test_compress.py
+++ tests/unit/test_compress.py
@@ -7,6 +7,7 @@ import sorbic.db
 
 # Import python libs
 import os
+import random
 import shutil
 import unittest
 import tempfile
@@ -51,4 +52,31 @@ class TestCompress(unittest.TestCase):
         for num in xrange(entries):
             pull_data = db.get(key)
             self.assertEqual(data, pull_data)
+        shutil.rmtree(w_dir)
+
+    def test_compress_changes(self):
+        '''
+        Compress a db with removed keys
+        '''
+        entries = 100
+        w_dir = tempfile.mkdtemp()
+        root = os.path.join(w_dir, 'db_root')
+        db = sorbic.db.DB(root)
+        rands = set()
+        data = {1:1}
+        for num in xrange(entries):
+            key = str(num)
+            db.insert(key, data)
+        for _ in xrange(entries):
+            rands.add(random.randint(0, entries))
+        for key in rands:
+            db.rm(str(key))
+        db.compress('', 0)
+        for num in xrange(entries):
+            key = str(num)
+            pull_data = db.get(key)
+            if num in rands:
+                self.assertIsNone(pull_data)
+            else:
+                self.assertEqual(data, pull_data)
         shutil.rmtree(w_dir)
